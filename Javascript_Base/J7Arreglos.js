6a76290b2b4461f6616f7c63153c1cc7dd7d31ed
Atzakell <ysaac356abaddon@gmail.com>
2022-06-30 16:09:46 -0500

    javascript teorico 0w0

---
+++ Javascript_Base/J7Arreglos.js
@@ -0,0 +1,30 @@
+//definir 
+const a=[];
+const b=[1,true,'Unreal Engine',['A',' Blender',[1,' Photoshop','maya']]];
+console.log(a);
+console.log(b);         //mostrar todo el arreglo
+console.log(b[2]);      //mostrar la posicion 2 del arreglo
+console.log(b[3][2][1]);//mostrar la pisicion 3 del sublemento 2 del subelemento 1
+
+const c =Array.of("x","y","z",9,8,7);
+console.log(c);
+
+//llenar arreglos con un valor
+const d = Array(100).fill(false);
+console.log(d);
+
+//formas que ya no se usan
+const e = new Array();
+console.log(e);
+
+const f=new Array(1,2,3,true,false)
+console.log(f);
+
+const colores=['Rojo','Verde','Azul'];
+console.log(colores);
+
+colores.push('Morado')  //agregar un elemento al final
+console.log(colores);
+
+colores.pop();  //quita el ultimo elemento
+console.log(colores);
---
+++ Javascript_Base/J8Objetos.js
@@ -0,0 +1,51 @@
+
+let a=new String('Hola');
+console.log(a);
+
+const b={}
+console.log(b);
+
+const c=new Object();
+console.log(c);;
+
+
+//dentro de un objetos sus variables se denominaran atributos
+//dentro de un objeto sus funciones se denominan metodos
+const LilAtza={
+  nombre: 'Lil-Ysaac',
+  cargo: 'Developer Dreamer',
+  edad:21,
+  dominios: ['Blender', 'Photoshop', 'Unreal Engine 5'],
+  soltero: true,
+  contacto:{
+    email:'ysaac356abaddon@gmail.com',
+    twitter: '@ysaac23',
+    celular: '991197158'
+  },
+  saludar: function(){
+    console.log(`Hola :D`);
+  },
+  presentarse: function(){
+  console.log(`Me llamo ${this.nombre} soy ${this.cargo} y tengo ${this.edad}`);
+  }
+}
+
+//invokar el objeto
+//console.log(LilAtza);
+console.log(LilAtza.nombre);
+console.log(LilAtza.cargo);
+console.log(LilAtza.edad);
+console.log(LilAtza.dominios);
+console.log(LilAtza.soltero);
+console.log(LilAtza.dominios[1]);
+console.log(LilAtza.contacto.email);
+LilAtza.saludar();
+LilAtza.presentarse();
+
+//Muestra los atributos y metodos del objeto
+console.log(Object.keys(LilAtza));
+//Muestra los valores de los abributos y metodos
+console.log(Object.values(LilAtza));
+//Comprueba si el atributo o metodo existe dentro del objeto
+console.log(LilAtza.hasOwnProperty('dominios'));
+console.log(LilAtza.hasOwnProperty('identidad'));
